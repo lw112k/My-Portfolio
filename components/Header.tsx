38024154cb6dfb57ee659e035c0af74da333e911
Lucaswlh <lwlh1103@gmail.com>
2026-02-24 14:32:26 +0800

    Misalign div

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import NavBar from "./NavBar";
 
 export default function Header() {
     return (
-                <div className="py-3 rounded-md mx-15 mt-5">
+        <div className="py-3 rounded-md mx-15 mt-5">
             <nav className="container mx-auto flex justify-between items-center text-white">
                 <Link href={"/"}>
                     <h1 className="text-4xl text-cape-cod-700 font-semibold border-3 rounded-full w-15 h-15 items-center justify-center flex">LW</h1>
