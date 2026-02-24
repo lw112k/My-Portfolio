45d496791436f3ddf69618dec5e35b0ee6754e0f
Lucaswlh <lwlh1103@gmail.com>
2026-02-24 11:29:33 +0800

    Create Footer

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+
+export default function Footer() {
+    const currentYear = new Date().getFullYear();
+    return (
+        <footer className="flex justify-center">
+            <p>Copyright &copy; {currentYear} Lucas Wong Li Hong</p>
+        </footer>
+    )
+}
