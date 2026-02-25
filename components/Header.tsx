9e5f0372c75b8aaeb895bb3a74383571b6c29d6a
Lucaswlh <lwlh1103@gmail.com>
2026-02-25 18:28:47 +0800

    Fix Sticky NavBar issues

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,10 +4,10 @@ import NavBar from "./NavBar";
 
 export default function Header() {
     return (
-        <div className="py-3 rounded-md mx-15 mt-5">
-            <nav className="container mx-auto flex justify-between items-center text-white">
+        <div className="py-3 fixed top-0 z-50 w-full bg-white/70 backdrop-blur-md border-b border-white/20">
+            <nav className="container mx-auto flex justify-between items-center text-white px-4">
                 <Link href={"/"}>
-                    <h1 className="text-4xl text-cape-cod-700 font-semibold border-3 rounded-full w-15 h-15 items-center justify-center flex">LW</h1>
+                    <h1 className="text-4xl text-cape-cod-700 font-semibold border-3 border-cape-cod-700 rounded-full w-15 h-15 items-center justify-center flex">LW</h1>
                 </Link>
                 <div className="flex xl-flex items-center gap-8">
                     <NavBar />
