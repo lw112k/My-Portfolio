ef83d28b2c327757f6f002983dde0d8c10c3a4cd
Lucaswlh <lwlh1103@gmail.com>
2026-02-25 18:27:22 +0800

    Changes for Smooth Scrolling scale issues

--- components/about.tsx
+++ components/about.tsx
@@ -1,5 +1,7 @@
 export default function About() {
     return (
-        <div>About</div>
+        <section className="min-h-screen w-full flex items-center justify-center scroll-mt-20">
+            <div>About</div>
+        </section>
     );
 }
--- components/service.tsx
+++ components/service.tsx
@@ -1,5 +1,7 @@
 export default function Service() {
     return (
-        <div>Service</div>
+        <section className="min-h-screen w-full flex items-center justify-center scroll-mt-20">
+            <div>Service</div>
+        </section>
     );
 }
