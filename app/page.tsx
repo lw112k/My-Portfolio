4de3d371d042566874e3544d8a6224f6c0b67fb0
Lucaswlh <lwlh1103@gmail.com>
2026-02-25 16:09:36 +0800

    Rename page for Single landing page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
 
+ 
 export default function Home() {
   return (
-    <div className="font-medium text-3xl">Hello</div>
+    <main>
+      <div className="">
+        Hompage
+      </div>
+    </main>
   );
 }
